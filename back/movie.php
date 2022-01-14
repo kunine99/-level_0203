0f9adaee9a385ead83d83fbdc22f5f2c6a8b0910
rong <rongyy94job@gmail.com>
2022-01-14 16:16:02 +0800

    完成院線片排序功能

--- back/movie.php
+++ back/movie.php
@@ -8,25 +8,19 @@
 $mos=$Movie->all(" ORDER BY rank");
 
 foreach ($mos as $key => $movie) {
-// back的poster.php複製來的
-$mos=$Poster->all(" ORDER by `rank`");
-            foreach($mos as $key => $row){
-                $checked=($row['sh']==1)?"checked":"";
-                // mos是二微陣列，每一筆都慧倫一次把變數給row
-                // 但沒有key 來當index(索引值?)? 
             if($key==0){
-                $up=$row['id'] . "-" . $row['id'];
-                $down=$row['id'] . "-" . $mos[$key+1]['id'];
+                $up=$movie['id'] . "-" . $movie['id'];
+                $down=$movie['id'] . "-" . $mos[$key+1]['id'];
             }
 
             if($key==(count($mos)-1)){
-                $down=$row['id'] . "-" . $row['id'];
-                $up=$row['id'] . "-" . $mos[$key-1]['id'];
+                $down=$movie['id'] . "-" . $movie['id'];
+                $up=$movie['id'] . "-" . $mos[$key-1]['id'];
             }
 
             if($key>0 && $key<(count($mos)-1)){
-               $up=$row['id'] . "-" . $mos[$key-1]['id'];
-               $down=$row['id'] . "-" . $mos[$key+1]['id'];
+               $up=$movie['id'] . "-" . $mos[$key-1]['id'];
+               $down=$movie['id'] . "-" . $mos[$key+1]['id'];
             }
 
 
@@ -50,9 +44,8 @@ $mos=$Poster->all(" ORDER by `rank`");
     <button class="show" data-id="<?=$movie['id'];?>">
             <?=($movie['sh']==1)?"顯示":"隱藏";?>
         </button>
-        <button>往上</button>
-        <button>往下</button>
-        <button>編輯電影</button>
+        <button class="sw" data-sw="<?=$up;?>">往上</button>
+        <button class="sw" data-sw="<?=$down?>">往下</button>
         <button onclick="location.href='?do=edit_movie&id=<?=$movie['id'];?>'">編輯電影</button>
         <button onclick="del('movie',<?=$movie['id'];?>)">刪除電影</button>
 
@@ -67,9 +60,18 @@ $mos=$Poster->all(" ORDER by `rank`");
 }
 ?>
 </div>
-</div>
+
 
 <script>
+    
+$('.sw').on('click',function(){
+        let id=$(this).data("sw").split("-");
+        $.post("api/sw.php",{id,table:"movie"},()=>{
+            location.reload();
+        })
+    })
+
+
 $(".show").on("click",function(e){
     let id=$(e.target).data("id");
     $.post("api/show.php",{id},()=>{
