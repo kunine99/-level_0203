ce362255de71830b33db7b4b1bdfc623b3160a3f
rong <rongyy94job@gmail.com>
2022-01-14 15:48:11 +0800

    完成院線片清單列表 修bug

--- back/movie.php
+++ back/movie.php
@@ -3,11 +3,6 @@
 
 <button onclick="location.href='?do=add_movie'">新增電影</button>
 <hr>
-<?php
-$mos=$Movie->all(" ORDER BY rank");
-
-?>
-
 <div style="overflow:auto;height:420px;">
 <?php
 $mos=$Movie->all(" ORDER BY rank");
@@ -19,13 +14,14 @@ foreach ($mos as $key => $movie) {
 <div style="display:flex;width:100%;">
 <div style="width:10%">
     <img src="img/<?=$movie['poster'];?>" style="width:100%">
+</div>
 <div style="width:10%">
     分級:
     <img src="icon/<?=$movie['level'];?>.png">
 </div>
 <div style="width:80%">
     <div style="display:flex">
-    <div style="width:33%">片名:<?=$movie['name'];?></div>
+        <div style="width:33%">片名:<?=$movie['name'];?></div>
         <div style="width:33%">片長:<?=$movie['length'];?></div>
         <div style="width:33%">上映時間:<?=$movie['ondate'];?></div>
     </div>
@@ -35,9 +31,16 @@ foreach ($mos as $key => $movie) {
         <button>往下</button>
         <button>編輯電影</button>
         <button>刪除電影</button>
+
     </div>
     <div>
-    劇情介紹：<?=$movie['intro'];?>
+        劇情介紹：<?=$movie['intro'];?>
     </div>
 </div>
+</div>
+<hr>
+<?php
+}
+?>
+
 </div>
