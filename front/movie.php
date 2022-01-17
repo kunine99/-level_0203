fe61a753fd008e957ddfaec26b9d959b36ff714a
rong <rongyy94job@gmail.com>
2022-01-17 10:49:17 +0800

    完成院線片清單分頁功能

--- front/movie.php
+++ front/movie.php
@@ -24,10 +24,17 @@
             $today = date("Y-m-d");
             $ondate = date("Y-m-d", strtotime("-2 days"));
             //分頁
-
+            $all=$Movie->math('count','*'," where `sh`=1 && `ondate` BETWEEN '$ondate' AND '$today'");
+            $div=4;
+            $pages=ceil($all/$div);
+            $now=$_GET['p']??1;
+            $start=($now-1)*$div;
+            $rows=$Movie->all(" where `sh`=1 && `ondate` BETWEEN '$ondate' AND '$today' 
+                                Order By `rank` 
+                                limit $start,$div");
 
             /* between 0(比較小的參數) and  0(比較大的參數) */
-            $rows = $Movie->all(" where `sh`=1 && `ondate` BETWEEN '$ondate' AND '$today' Order By `rank`");
+            // $rows = $Movie->all(" where `sh`=1 && `ondate` BETWEEN '$ondate' AND '$today' Order By `rank`");
             foreach ($rows as $key => $row) {
             ?>
                 <div>
@@ -50,9 +57,31 @@
             }
             ?>
         </div>
-
         <div class="ct">
-            < 1 2 3>
+            <?php
+            if(($now-1)>=1){
+                $prev=$now-1;
+                echo "<a href='index.php?p=$prev'> ";
+                echo " < ";
+                echo " </a>";
+            }
+
+            for($i=1;$i<=$pages;$i++){
+                $size=($now==$i)?"24px":"16px";
+                echo "<a href='index.php?p=$i' style='font-size:$size'> ";
+                echo $i;
+                echo " </a>";
+            }
+
+            if(($now+1)<=$pages){
+                $next=$now+1;
+                echo "<a href='index.php?p=$next' > ";
+                echo " > ";
+                echo " </a>";
+            }
+
+
+            ?>
         </div>
     </div>
 </div>
