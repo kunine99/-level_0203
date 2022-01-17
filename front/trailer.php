cd682209474ffab75d4809862cf94593ec19c68e
rong <rongyy94job@gmail.com>
2022-01-17 16:16:20 +0800

    製造下方案水平移動動畫

--- front/trailer.php
+++ front/trailer.php
@@ -8,13 +8,16 @@
   height:260px;
   margin:auto;
   overflow:hidden;
+  position:relative;
 }
 .lists .po{
   width:100%;
   text-align:center;
   display:none;
+  position:absolute;
 }
-.po img{
+.po img,
+.icon img{
   width:100%;
   border:2px solid white;
 }
@@ -28,23 +31,27 @@
 .icons {
     display: flex;
     width: 320px;
-    height: 90px;
+    height: 110px;
+    overflow:hidden;
+    font-size:12px;
 }
 .icon{
   width:80px;
-  height:20px;
+  flex-shrink:0;
+  padding:5px;
+  position:relative;
 }
 .left {
-  border-top:20px solid transparent;
-  border-right:25px solid black;
-  border-bottom:20px solid  transparent;
-   /* border-left:25px solid  transparent; */
-   cursor: pointer;
+    border-top:20px solid transparent;
+    border-right:25px solid black;
+    border-bottom:20px solid  transparent;
+    /* border-left:25px solid  transparent; */
+    cursor: pointer;
 }
 .right {
   border-top:20px solid transparent;
     /* border-right:25px solid transparent; */
-    border-bottom:20px solid transparent;
+    border-bottom:20px solid  transparent;
     border-left:25px solid  black;
     cursor: pointer;
 }
@@ -56,36 +63,98 @@
 }
 </style>
 <div class="half" style="vertical-align:top;">
-  <h1>預告片介紹</h1>
-  <div class="rb tab" style="width:95%;">
-    <div>
-    <div class="lists">
-    <?php
+      <h1>預告片介紹</h1>
+      <div class="rb tab" style="width:95%;">
+        <div>
+          <div class="lists">
+            <?php
               $pos=$Poster->all(" where `sh`=1 Order By `rank`");
               foreach ($pos as $key => $po) {
-                echo "<div class='po'>";
+                echo "<div class='po' data-ani='{$po['ani']}'>";
                 echo "<img src='img/{$po['path']}'>";
                 echo $po['name'];
 
                 echo "</div>";
               }
-
             ?>
           </div>
           <div class="controls">
             <div class="left"></div>
             <div class="icons">
-                <div class="icon">sss</div>
-                <div class="icon">sss</div>
-                <div class="icon">sss</div>
-                <div class="icon">sss</div>
+              <?php
+              foreach ($pos as $key => $po) {
+                echo "<div class='icon' data-ani='{$po['ani']}'>";
+                echo "<img src='img/{$po['path']}'>";
+                echo $po['name'];
+                echo "</div>";
+              }
+              ?>
             </div>
             <div class="right"></div>
           </div>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
 <script>
+  // 拿到值要給變數才能切換？
 $(".po").eq(0).show();
+let i=0;
+let all=$('.po').length; 
+
+let slides=setInterval(() => {
+    i++;
+    if(i>all-1){
+      i=0;
+    }
+    ani(i);
+
+}, 2500);
+
+
+function ani(n){
+  let ani=$(".po").eq(n).data('ani');
+  let now=$(".po:visible")
+  let next=$(".po").eq(n)
+
+  switch(ani){
+    case 1:
+      //淡入淡出
+      now.fadeOut(1000);
+      next.fadeIn(1000);
+    break;
+    case 2:
+    //縮放
+      now.hide(1000,function(){
+      next.show(1000);
+    });
+    break;
+    case 3:
+      //滑入滑出
+      now.slideUp(1000,function(){
+        next.slideDown(1000);
+      });
+    break;
+  }
+}
+
+let p=0;
+$(".left,.right").on("click",function(){
+    if($(this).hasClass('left')){
+      if(p-1>=0){
+        p--;
+      }
+
+    }else{
+      if(p+1<=all-4){
+        p++;
+      }
+    }
+    
+    // 第一個參數是css屬性，第二個時間，第三個是回乎函式
+    $(".icon").animate({right:p*80},500)
+})
 
 </script>    
+// 拿到值要給變數才能切換
+$(".po").eq(0).show(); //eq(0) 第一個
+
