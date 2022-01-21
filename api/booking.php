849e540387ce64dc599dd7e3cef2892e033407b3
rong <rongyy94job@gmail.com>
2022-01-21 15:40:28 +0800

    建構座位表

--- api/booking.php
+++ api/booking.php
@@ -5,8 +5,65 @@ $date=$_GET['date'];
 $session=$ss[$_GET['session']];
 ?>
 
+<!-- 等於20會跑21個，所以<20才對 -->
 
-<div>
+
+<style>
+    #seats {
+        display: flex;
+        flex-wrap: wrap;
+        width:540px;
+        height:370px;
+        padding:19px 112px 12px 112px;
+        margin:auto;
+        background:url('icon/03D04.png');
+        box-sizing:border-box;
+    }
+    
+    .seat {
+        width: 63px;
+        height: 85px;
+        position:relative;
+    }
+    
+    .null{
+        background:url('icon/03D02.png');
+        background-position:center;
+        background-repeat:no-repeat;
+    }
+    
+    .booked{
+        background:url('icon/03D03.png');
+        background-position:center;
+        background-repeat:no-repeat;
+    }
+    
+    
+    .check{
+        position:absolute;
+        right:5px;
+        bottom:5px;
+    }
+    </style>
+    
+<!-- 如果我點下去的屬性是true的話我就做這些事情 -->
+<!-- 如果不是我就從陣列裡面刪除 -->
+
+<div id="seats">
+    <?php
+
+for($i=0;$i<20;$i++){
+    echo "<div class='seat null'>";
+    echo "  <div class='ct'>";
+    echo    (floor($i/5)+1). "排".($i%5 +1)."號";
+    echo "  </div>";
+    echo "<input type='checkbox' name='check' class='check' value='$i'>";
+    echo "</div>";
+}
+?>
+</div>
+
+<div style="width:540px;margin:auto">
     <div>您選擇的電影是：<?=$movie['name'];?></div>
     <div>您選擇的時刻是：<?=$date;?> <?=$session;?></div>
     <div>您已經勾選了<span id="tickets"></span>張票，最多可以購買四張票</div>
