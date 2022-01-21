48e4335b3f5bd5df0c8b2f11885e9d63ecf155f9
rong <rongyy94job@gmail.com>
2022-01-21 16:34:24 +0800

    解決過日場次不能訂票

--- api/get_sessions.php
+++ api/get_sessions.php
@@ -5,7 +5,16 @@ $date=$_GET['date'];
 
 
 
-for($i=1;$i<=5;$i++){
+if($date!=date("Y-m-d") || date("G")<14){
+    // 5場
+    $s=5;
+}else{
+    $s=5-ceil((date("G")-13)/2);
+}
+
+
+// 開始的場次=當下時間的場次 用6減$S 
+for($i=(6-$s);$i<=5;$i++){
     $seats=$Ord->math('sum','qt',['movie'=>$movie['name'],'date'=>$date,'session'=>$ss[$i]]);
     echo "<option value='$i'>{$ss[$i]} 剩餘座位 ".(20-$seats)."</option>";
 }
