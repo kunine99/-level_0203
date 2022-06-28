178b58e91e783a640beaa5cfa7a2d24927ccbab9
enyo <rongyy94job@gmail.com>
2022-06-28 19:44:23 +0800

    完成電影介紹頁

--- front/intro.php
+++ front/intro.php
@@ -1,30 +1,41 @@
-﻿
-  <div id="mm">
-    <div class="tab rb" style="width:87%;">
-      <div style="background:#FFF; width:100%; color:#333; text-align:left">
-        <video src="movie/03B20v.avi" width="300px" height="250px" controls="" style="float:right;"></video>
-        <font style="font-size:24px"> <img src="Profile page_files/03B20.png" width="200px" height="250px" style="margin:10px; float:left">
-        <p style="margin:3px">影片名稱 ：
-          <input type="button" value="線上訂票" onclick="lof(&#39;?do=ord&amp;id=4&#39;)" style="margin-left:50px; padding:2px 4px" class="b2_btu">
-        </p>
-        <p style="margin:3px">影片分級 ： <img src="Profile page_files/03C04.png" style="display:inline-block;">限制級 </p>
-        <p style="margin:3px">影片片長 ： 時/分</p>
-        <p style="margin:3px">上映日期 2014/02/14</p>
-        <p style="margin:3px">發行商 ： </p>
-        <p style="margin:3px">導演 ： </p>
-        <br>
-        <br>
-        <p style="margin:10px 3px 3px 3px; word-break:break-all"> 劇情簡介：<br>
-        </p>
-        </font>
-        <table width="100%" border="0">
-          <tbody>
-            <tr>
-              <td align="center"><input type="button" value="院線片清單" onclick="lof(&#39;?&#39;)"></td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </div>
+﻿<?php
+// 帶id進來
+$row = $Movie->find($_GET['id']);
+?>
+<div class="tab rb" style="width:87%;">
+  <div style="background:#FFF; width:100%; color:#333; text-align:left">
+    <!-- 影片檔名 -->
+    <video src="img/<?= $row['trailer']; ?>" width="300px" height="250px" controls="" style="float:right;"></video>
+    <font style="font-size:24px"> 
+    <!-- 海報 -->
+    <img src="img/<?= $row['poster']; ?>" width="200px" height="250px" style="margin:10px; float:left">
+      <p style="margin:3px">影片名稱 ：<?= $row['name']; ?>
+      <!-- 線上訂票連結 -->
+        <input type="button" value="線上訂票" onclick="location.href='?do=order&id=<?= $row['id']; ?>'" style="margin-left:50px; padding:2px 4px" class="b2_btu">
+      </p>
+      <p style="margin:3px">影片分級 ： <img src="icon/<?= $row['level']; ?>.png" style="display:inline-block;">
+      <!--請他到$Movie去找level的functionb,然後把我的level帶進去就會得到相應的文字   -->
+      <?= $Movie->level($row['level']); ?></p>
+      <!-- 在所有的程式語言中,取小時和剩下的分鐘的做法幾乎都一樣 因為這是常識
+           除法+餘數感覺比較完整,一個算式就解決 -->
+      <p style="margin:3px">影片片長 ：<?= floor($row['length']/60); ?>小時<?= $row['length']%60; ?>分</p>
+      <!-- 如果很無聊想把-改成/ 就用str_replace -->
+      <p style="margin:3px">上映日期 ：<?= $row['ondate']; ?></p>
+      <p style="margin:3px">發行商 ：<?= $row['publish']; ?> </p> 
+      <p style="margin:3px">導演 ：<?= $row['director']; ?> </p>
+      <br>
+      <br>
+      <p style="margin:10px 3px 3px 3px; word-break:break-all"> 劇情簡介：<br>
+        <?= $row['intro']; ?>
+      </p>
+    </font>
+    <table width="100%" border="0">
+      <tbody>
+        <tr>
+          <!-- 回到首頁(院線片清單) -->
+          <td align="center"><input type="button" value="院線片清單" onclick="location.href='index.php'"></td>
+        </tr>
+      </tbody>
+    </table>
   </div>
- 
+</div>
