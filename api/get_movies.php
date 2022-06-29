f63d9e639f956f548366ed5bdc55298bff7b6a9b
enyo <rongyy94job@gmail.com>
2022-06-29 21:06:45 +0800

    建置電影訂單畫面

---
+++ api/get_movies.php
@@ -0,0 +1,27 @@
+<?php
+include_once "../base.php";
+// 我要撈出來的電影有哪些?
+// 基本上跟首頁顯示出來的院線片清單一樣
+// (現實中可能可以訂到沒有在首頁的院線片清單的電影,不過這題不會)
+// 接下來複製front/movie.php裡列出所有movie的條件式
+// ↑這個條件式不需要分頁也不需要排序所以後面的條件可以刪除
+// 記得$today跟$ondate也要複製 
+
+$today = date("Y-m-d");
+$ondate = date("Y-m-d", strtotime("-2 days"));
+
+$movies = $Movie->all(" where `sh`=1 && `ondate` BETWEEN '$ondate' AND '$today'");
+//我們這段期間(上印期間)的電影要把它做成選項丟給前端
+//讓他插入到我們的電影裡面去
+foreach ($movies as $movie) {
+    // 他會幫我們產生5部電影的清單,這個清單裡面要寫什麼呢?
+    //通常是放我電影裡面的$movie的id跟名稱
+    echo "<option value='{$movie['id']}'>{$movie['name']}</option>";
+    
+}
+
+//當我有個請求到api這邊,get movies要電影的資料
+//我就會用這個方式倒出這段期間會上印的電影
+//然後做成一個選項的清單,丟給前端
+//前端收到之後,script就會幫我丟到id movie的select裡面去
+//這樣我就會產生清單了
--- front/order.php
+++ front/order.php
@@ -1,1 +1,59 @@
-訂票
+<!-- 連動式選單 -->
+<h3 class='ct'>線上訂票</h3>
+<style>
+    #order{
+        width:50%;
+        margin:auto;
+    }
+    .row{
+        display:flex;
+        width:100%;
+    }
+    /* 子選擇器 這樣寫也可以.row div:first-child
+       .row下面的div的第一個 */
+    .row .first{
+        width:15%;
+        text-align:right;
+    }
+    /* 子選擇器 這樣寫也可以.row div:nth-child(2)*/
+    .row .sec{
+        width:85%;
+        text-align:left;
+    }
+    /* 子選擇器 這樣寫也可以.row:nth-last-child(0) div ???*/
+    .sec select{
+        width:100%;
+    }
+
+</style>
+<div id="order">
+    <!-- div.row*4>div+div>select -->
+<div class="row">
+    <div class="first">電影：</div>
+    <!-- 要用ajax的作法m所以input這邊都要記得加上id -->
+    <div class="sec"><select name="movie" id="movie"></select></div>
+</div>
+<div class="row">
+    <div class="first">日期：</div>
+    <div class="sec"><select name="date" id="date"></select></div>
+</div>
+<div class="row">
+    <div class="first">場次：</div>
+    <div class="sec"><select name="session" id="session"></select></div>
+</div>
+<div class="row">
+    <div class="ct" style="width:100%">
+        <button>確定</button><button>重置</button>
+    </div>
+    
+</div>
+</div>
+<script>
+    //應該要有一個程式(=api)讓我可以拿到電影的東西,要帶什麼參數去呢?
+    //原則上是不需要帶參數,我現在只是想把我所有可以訂的電影的票.....?
+    //他會回傳我電影的列表(movies)回傳之後我就把它放到我的畫面上
+    $.get("api/get_movies.php",(movies)=>{
+    // 當我的畫面載入的時候,就去我的#movie放入movies
+    $("#movie").html(movies)
+})
+</script>
